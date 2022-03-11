9674d8df1559ca50ea95dd9c8e31606c354539ab
ykakkaya <100940437+ykakkaya@users.noreply.github.com>
2022-03-11 11:20:49 +0300

    Add files via upload

--- OrtalamaHesapla.java
+++ OrtalamaHesapla.java
@@ -3,25 +3,23 @@ import java.util.*;
 public class OrtalamaHesapla {
 
     public static void main(String[] args) {
-        Scanner s= new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         System.out.println("matematik notunuzu giriniz");
-        float math= s.nextFloat();
+        float math = s.nextFloat();
         System.out.println("fizik notunuzu giriniz");
-        float fizik= s.nextFloat();
+        float fizik = s.nextFloat();
         System.out.println("kimya notunuzu giriniz");
-        float kimya= s.nextFloat();
+        float kimya = s.nextFloat();
         System.out.println("türkçe notunuzu giriniz");
-        float turkce= s.nextFloat();
+        float turkce = s.nextFloat();
         System.out.println("tarih notunuzu giriniz");
-        float tarih= s.nextFloat();
+        float tarih = s.nextFloat();
         System.out.println("müzik notunuzu giriniz");
-        float muzik= s.nextFloat();
-        float avg= (math+fizik+kimya+turkce+tarih+muzik)/6;
-        System.out.println("ortalamanız  ="+avg);
-        if ((avg>=60) && ( avg<=100)){
-            System.out.println("sınıfı geçtiniz");
-        }else{
-            System.out.println("sınıfta kaldınız daha çok çalışmalısınız");
-        }
+        float muzik = s.nextFloat();
+        float avg = (math + fizik + kimya + turkce + tarih + muzik) / 6;
+        System.out.println("ortalamanız  =" + avg);
+       
+        String durum = (avg >= 60) ? "geçtiniz" : "kaldınız";
+        System.out.println(durum);
     }
 }
