02f966bc61cb0067c9069668ec17e6feb95be16a
ykakkaya <100940437+ykakkaya@users.noreply.github.com>
2022-03-11 10:50:23 +0300

    Add files via upload

---
+++ OrtalamaHesapla.java
@@ -0,0 +1,27 @@
+package com.company;
+import java.util.*;
+public class OrtalamaHesapla {
+
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        System.out.println("matematik notunuzu giriniz");
+        float math= s.nextFloat();
+        System.out.println("fizik notunuzu giriniz");
+        float fizik= s.nextFloat();
+        System.out.println("kimya notunuzu giriniz");
+        float kimya= s.nextFloat();
+        System.out.println("türkçe notunuzu giriniz");
+        float turkce= s.nextFloat();
+        System.out.println("tarih notunuzu giriniz");
+        float tarih= s.nextFloat();
+        System.out.println("müzik notunuzu giriniz");
+        float muzik= s.nextFloat();
+        float avg= (math+fizik+kimya+turkce+tarih+muzik)/6;
+        System.out.println("ortalamanız  ="+avg);
+        if ((avg>=60) && ( avg<=100)){
+            System.out.println("sınıfı geçtiniz");
+        }else{
+            System.out.println("sınıfta kaldınız daha çok çalışmalısınız");
+        }
+    }
+}
