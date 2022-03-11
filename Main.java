acb6f79a260b615399a685372875cacc402c4452
ykakkaya <100940437+ykakkaya@users.noreply.github.com>
2022-03-11 12:23:47 +0300

    UcgenAlanıHesapla
    ucgenin alanı

--- Main.java
+++ Main.java
@@ -1,15 +1,19 @@
 package com.company;
 import java.util.*;
-import java.math.*;
 public class Main {
 
     public static void main(String[] args) {
 	Scanner scanner=new Scanner(System.in);
-    System.out.println("birinci dik kenarı giriniz");
-    int kenar1= scanner.nextInt();
-    System.out.println("ikinci dik kenarı giriniz");
-    int kenar2= scanner.nextInt();
-    double hipotenus=(double)Math.sqrt((kenar1*kenar1)+(kenar2*kenar2));
-    System.out.println("hipotenus değerğimiz :"+hipotenus);
+    System.out.println("birinci kenarı giriniz");
+    int first= scanner.nextInt();
+    System.out.println("ikinci kenarı giriniz");
+    int second= scanner.nextInt();
+    System.out.println("üçüncü kenarı giriniz");
+    int third= scanner.nextInt();
+    int u= (first+second+third)/2;
+    double alan= Math.sqrt(u* (u-first)*(u-second)*(u-third));
+    System.out.println("üçgenimizin alanı :"+alan);
+
+
     }
 }
