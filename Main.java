54b42ed8db3757d6f694a9d0e125929262942384
ykakkaya <100940437+ykakkaya@users.noreply.github.com>
2022-03-16 11:57:14 +0300

    HarmonikSıralama
    verilen değere kadar harmonik sıralama hesaplar

--- Main.java
+++ Main.java
@@ -3,22 +3,13 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	    Scanner scanner=new Scanner(System.in);
-        int number=scanner.nextInt();
-        int counter=0;
-        int degree=0;
-        int result=0;
-        while(number!=0){
-
-            degree=number%10;
-
-            number/=10;
-
-            result+=degree;
-
-        }
-        System.out.println("basamaklar toplamı :"+ result);
-
-
+	System.out.println("hangi sayının harmonik serisini bulmak istiyorsunuz");
+    Scanner scanner=new Scanner(System.in);
+    int number=scanner.nextInt();
+    double result=0.0;
+    for (double i=1; i<=number;i++){
+        result += (1/i);
+    }
+    System.out.println("harmonik seri sonucu :"+ result);
     }
 }
