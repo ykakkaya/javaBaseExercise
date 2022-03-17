8eadebd008dd92654aa2eb9afd0e84c5db74c6b3
ykakkaya <100940437+ykakkaya@users.noreply.github.com>
2022-03-17 10:41:29 +0300

    ATM PROJESİ
    KULLANICI ADI ŞİFRE KONTROLÜ
    PARA YATIRMA-ÇEKME-BAKİYE SORMA ÇIKIŞ İŞLEMLERİ
    PAROLA 3 KERE YANLIŞSA BLOKE 
    KULLANICI ADI : java şifre 123456

--- Main.java
+++ Main.java
@@ -1,21 +1,75 @@
 package com.company;
 import java.util.*;
 public class Main {
+    static int bakiye;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("kaç basamaklı olacak");
-        int basamak = scanner.nextInt();
+        Scanner scanner=new Scanner(System.in);
+        String userName="java";
+        String password="123456";
 
-        for (int i = 1; i <= basamak; i++) {
-            for (int k = 1; k <= (basamak - i); k++) {
-                System.out.print(" ");
+        int deneme=1;
+        while(deneme<=3){
+            System.out.println("kullanıcı adınızı giriniz");
+            String user= scanner.nextLine();
+            System.out.println("şifrenizi giriniz");
+            String pass=scanner.nextLine();
+            if (userName.equals(user)&& password.equals(pass)) {
+                System.out.println("giriş işlemi başarılı");
+                int select = 2;
+                while (select > 1 && select <4) {
+
+                    System.out.println("hangi işlemi yapmak istersiniz :" +
+                            " 1 para yatırma  2 para çekme  3 bakiye sorma  4 çıkış");
+                    int select2 = scanner.nextInt();
+                    if (select2==4){
+                        System.out.println("çıkış işlemi yapıldı");
+                        select=0;
+                        deneme=4;
+                    }
+                        switch (select2) {
+                            case 1:
+                            paraYatır();
+                            break;
+                            case 2:
+                            paraCek();
+                            break;
+                            case 3:
+                            paraBakiye();
+                            break;
+
+                        }
+                }
+            }else{
+                if (deneme==3){
+                    System.out.println("kartınız bloke oldu bankanızla görüşün");
+                }
+                deneme++;
             }
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                System.out.print("*");
-            }
-            System.out.println(" ");
         }
+	
+    }
 
+    public static void paraYatır(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("yatırılacak tutarı giriniz");
+        int tutar= scanner.nextInt();
+        bakiye+=tutar;
+        System.out.println("yatırılan tutar :"+ tutar);
+        System.out.println("toplam bakiye : "+ bakiye);
+    }
+    public static void paraCek(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("çekilecek tutarı giriniz");
+        int tutar= scanner.nextInt();
+        if(bakiye>=0 && bakiye>=tutar) {
+            bakiye -= tutar;
+        }
+        System.out.println("yatırılan tutar :"+ tutar);
+        System.out.println("toplam bakiye : "+ bakiye);
+    }
+    public static void paraBakiye(){
+
+        System.out.println("toplam bakiye : "+ bakiye);
     }
 }
