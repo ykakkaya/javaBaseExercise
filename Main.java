30c4fa50d1b4aa59ae89a0b7bd31c06787274034
ykakkaya <100940437+ykakkaya@users.noreply.github.com>
2022-03-31 13:57:33 +0300

    harfYazma
    * larla harf yazma

--- Main.java
+++ Main.java
@@ -1,38 +1,26 @@
 package com.company;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
+        String[][] letter = new String[6][4];
 
-            int [] list = {15,12,788,1,-1,-778,2,0};
-            int a;
-            int x=0;
-            int y=0;
-            Scanner input = new Scanner(System.in);
-
-            System.out.println("Dizi : " + Arrays.toString(list));
-            System.out.print("bir sayı giriniz : ");
-            a = input.nextInt();
-
-            Arrays.sort(list);
-
-            for (int i =list.length-1; i>=0;i--){
-                if(a>list[i]){
-                    x = list[i];
-                    break;
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if (i == 0 || i == 2) {
+                    letter[i][j] = " * ";
+                } else if (j == 0 || j == 3) {
+                    letter[i][j] = " * ";
+                } else {
+                    letter[i][j] = "   ";
                 }
             }
-            for (int j =0; j<list.length-1;j++){
-                if(a<list[j]){
-                    y = list[j];
-                    break;
-                }
+        }
+
+        for (String[] row : letter){
+            for (String col : row){
+                System.out.print(col);
             }
-            System.out.println("Girilen sayıdan küçük en yakın değer : " + x);
-            System.out.println("Girilen sayıdan büyük en yakın değer : " + y);
+            System.out.println();
         }
     }
-
+}
