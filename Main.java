62d1affd19584dde4d5bbff50e0f3a87bd5e4c38
Yakup Akkaya <100940437+ykakkaya@users.noreply.github.com>
2022-04-22 10:58:42 +0300

    transpoz
    transpoz

--- Main.java
+++ Main.java
@@ -1,15 +1,30 @@
 package com.company;
-import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Please enter n :");
-        int n = scanner.nextInt();
-        for(int i = 0; i < n;i++){
-            for(int j = 0;j<2*n;j++){
-                if(j<=i) System.out.print(" ");
-                else if (j<=n-i)System.out.print("*");
+        int[][] matris = {
+                {2, 3, 4},
+                {5, 6, 4}
+        };
+        int[][] transpose = new int[3][2];
+
+        System.out.println("Matris :");
+        for (int i = 0; i < matris.length; i++) {
+            for (int j = 0; j < matris[i].length; j++) {
+                System.out.print(matris[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Transpoze :");
+        for (int i = 0; i < transpose.length; i++) {
+            for (int j = 0; j < transpose[i].length; j++) {
+                transpose[i][j] = 0;
+                for (int k = 0; k < transpose.length; k++) {
+                    transpose[i][j] = matris[j][i];
+                }
+                System.out.print(transpose[i][j] + " ");
             }
             System.out.println();
 
